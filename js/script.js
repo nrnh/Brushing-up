66d58e009fad4f1b682bdcda7a4f4fe2994ee11f
nrnh <49420859+nrnh@users.noreply.github.com>
2020-08-20 14:20:06 +0800

    Add vue

--- js/script.js
+++ js/script.js
@@ -43,3 +43,48 @@ $(document).ready(function(){
 		$("#hov1").text("")
 	});
 });
+
+var emailRegExp = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+new Vue({
+  	// root node
+  	el: "#form",
+  	// the instance state
+  	data: function() {
+    	return {
+      		name: "Jane Doe",
+      		email: {
+        		value: "jd@hnd.oe",
+        		valid: true
+	      	},
+	      	message: {
+	        	text: "Hello, ...",
+	        	maxlength: 255
+	      	},
+	      	submitted: false
+	    };
+  	},
+  	methods: {
+    	// submit form handler
+    	submit: function() {
+      		this.submitted = true;
+    	},
+    	// validate by type and value
+    	validate: function(type, value) {
+      		if (type === "email") {
+        		this.email.valid = this.isEmail(value) ? true : false;
+      		}
+    	},
+    	// check for valid email adress
+    	isEmail: function(value) {
+      		return emailRegExp.test(value);
+    	},
+  	},
+  	watch: {
+    	// watching nested property
+    	"email.value": function(value) {
+      	this.validate("email", value);
+  		}
+  	}
+});
+
